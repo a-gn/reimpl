b13272dd1b2da63342df192c51947164082c9a0d
Arno Gobbin <32413451+a-gn@users.noreply.github.com>
2024-01-06 18:40:01 +0000

    Encoder level
    Signed-off-by: Arno Gobbin <32413451+a-gn@users.noreply.github.com>

--- src/voxels/bts.py
+++ src/voxels/bts.py
@@ -1,17 +1,44 @@
 """Re-implementation of Behind the Scenes (CVPR 23)."""
 
+from abc import ABC, abstractmethod
 from typing import Callable
 
 import jax.numpy as jnp
 import jax.random as random
 from jax import lax
+from jax.numpy import ndarray
 
 
 def relu(x: jnp.ndarray) -> jnp.ndarray:
     return jnp.maximum(x, 0)
 
 
-class UNet2DConvBlock:
+def maxpool2d(
+    x: jnp.ndarray,
+    window_size: tuple[int, int] = (2, 2),
+    strides: tuple[int, int] = (2, 2),
+) -> jnp.ndarray:
+    """Max pooling with a 2x2 window."""
+    return lax.reduce_window(
+        x,
+        init_value=jnp.zeros_like(x),
+        computation=jnp.max,
+        window_dimensions=window_size,
+        window_strides=strides,
+        padding="SAME",
+    )
+
+
+class TrainableModule(ABC):
+    """Module with parameters that can be trained."""
+
+    @abstractmethod
+    def get_trainable_parameters(self) -> list[jnp.ndarray]:
+        """Get all of this module's trainable parameters."""
+        pass
+
+
+class UNet2DConvBlock(TrainableModule):
     """One convolution + activation + normalization block in a U-Net."""
 
     def __init__(
@@ -34,3 +61,39 @@ class UNet2DConvBlock:
         x = lax.conv(image, self.kernel, window_strides=(1, 1), padding="SAME")
         x = self.activation(x)
         return x
+
+    def get_trainable_parameters(self) -> list[ndarray]:
+        return [self.kernel]
+
+
+class UNet2DEncoderLevel(TrainableModule):
+    """One level of the encoder in a U-Net.
+
+    Goes through multiple convolution-activation blocks.
+    Output always has the same spatial size as the input.
+
+    """
+
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        block_count: int = 3,
+        kernel_size: tuple[int, int] = (3, 3),
+        activation: Callable[[jnp.ndarray], jnp.ndarray] = relu,
+        init_key: random.KeyArray = random.key(0),
+    ):
+        self.levels = [
+            UNet2DConvBlock(in_chan, out_channels, kernel_size, activation, init_key)
+            for in_chan in [in_channels] + [out_channels] * (block_count - 1)
+        ]
+
+    def __call__(self, image: jnp.ndarray) -> jnp.ndarray:
+        """Compute the output of this level, with and without max-pooling."""
+        x = image
+        for level in self.levels:
+            x = level(x)
+        return x
+
+    def get_trainable_parameters(self) -> list[ndarray]:
+        return [p for level in self.levels for p in level.get_trainable_parameters()]
--- test/test_unet.py
+++ test/test_unet.py
@@ -1,8 +1,10 @@
+from typing import Any
+
 import jax.numpy as jnp
 import jax.random as random
 import pytest
 
-from voxels.bts import UNet2DConvBlock
+from voxels.bts import UNet2DConvBlock, UNet2DEncoderLevel
 
 KEY = random.key(0)
 
@@ -26,6 +28,35 @@ def test_pass_data_through_conv_block(
 ):
     block = UNet2DConvBlock(in_channels, out_channels, kernel_size, init_key=KEY)
     output = block(input_image)
+    assert output.shape == (
+        input_image.shape[0],
+        out_channels,
+        input_image.shape[2],
+        input_image.shape[3],
+    )
+
+
+@pytest.mark.parametrize(
+    ["in_channels", "out_channels", "other_encoder_block_args", "input_image"],
+    [
+        [
+            2,
+            1,
+            {},
+            jnp.ones((2, 2, 8, 8)),
+        ],
+    ],
+)
+def test_pass_data_through_encoder_level(
+    in_channels: int,
+    out_channels: int,
+    other_encoder_block_args: dict[str, Any],
+    input_image: jnp.ndarray,
+):
+    level = UNet2DEncoderLevel(
+        in_channels, out_channels, init_key=KEY, **other_encoder_block_args
+    )
+    output = level(input_image)
     assert output.shape == (
         input_image.shape[0],
         out_channels,
