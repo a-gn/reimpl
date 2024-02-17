6d6039eab0d5475a5304fd88408717d779282f13
Arno Gobbin <32413451+a-gn@users.noreply.github.com>
2024-02-17 13:04:19 +0000

    Create ECR repo
    Signed-off-by: Arno Gobbin <32413451+a-gn@users.noreply.github.com>

---
+++ .devcontainer/terraform.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.35.0"
+    }
+  }
+  backend "s3" {
+    bucket = "a-gn-cicd-data"
+    key    = "terraform/state/github-cicd/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+resource "aws_ecr_repository" "reimpl-dev-containers" {
+  name                 = "reimpl-dev-containers"
+  image_tag_mutability = "MUTABLE"
+}
