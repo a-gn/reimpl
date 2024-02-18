566310ca8ad484b4a9876709f704a637d2f19486
Arno Gobbin <32413451+a-gn@users.noreply.github.com>
2024-02-18 11:38:36 +0000

    Disable commit.gpgsign
    Signed-off-by: Arno Gobbin <32413451+a-gn@users.noreply.github.com>

--- .devcontainer/post-create-commands.sh
+++ .devcontainer/post-create-commands.sh
@@ -3,4 +3,5 @@
 git config --global user.name "Arno Gobbin"
 git config --global user.email "32413451+a-gn@users.noreply.github.com"
 git config --global user.signingkey "DE8F13577EB74FA7"
-git config --global commit.gpgsign true
+# Can't get gpg-agent to be forwarded to the container in VSCode
+# git config --global commit.gpgsign true
