417527ea2e1aa24a9a0e647b6996c3d90f351edb
Arno Gobbin <32413451+a-gn@users.noreply.github.com>
2024-02-18 11:26:24 +0000

    GPG, post-create script
    Signed-off-by: Arno Gobbin <32413451+a-gn@users.noreply.github.com>

---
+++ .devcontainer/post-create-commands.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+git config --global user.name "Arno Gobbin"
+git config --global user.email "32413451+a-gn@users.noreply.github.com"
+git config --global user.signingkey "DE8F13577EB74FA7"
+git config --global commit.gpgsign true
