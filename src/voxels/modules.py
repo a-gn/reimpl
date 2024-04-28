b3c0a7661555f898e22eb8952fdbfb9715345792
Arno Gobbin <32413451+a-gn@users.noreply.github.com>
2024-04-28 10:33:48 +0000

    Don't type TrainableModule.__call__
    We want to be able to return custom types.
    
    Signed-off-by: Arno Gobbin <32413451+a-gn@users.noreply.github.com>

--- src/voxels/modules.py
+++ src/voxels/modules.py
@@ -6,8 +6,6 @@ import jax.numpy as jnp
 class TrainableModule(Protocol):
     """Module with parameters that can be trained."""
 
-    __call__: Callable[..., jnp.ndarray]
-
     def get_trainable_parameters(self) -> list[jnp.ndarray]:
         """Get all of this module's trainable parameters."""
         ...
