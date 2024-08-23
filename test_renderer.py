c5f2409c9314b26ce226244918cd143b8286be1f
Arno Gobbin <32413451+a-gn@users.noreply.github.com>
2024-08-23 12:26:56 +0200

    don't divide cam params, add matplotlib, test script

--- test_renderer.py
+++ test_renderer.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import jax.numpy as jnp
 
 from reimpl_a_gn.voxels.render import (
@@ -10,11 +11,29 @@ from reimpl_a_gn.voxels.render import (
 camera_origin = jnp.array([0.0, 0.0, 0.0, 1.0])
 camera_params = jnp.array(
     [
-        [1.0, 0.0, 0.0, 1.0],
-        [0.0, 1.0, 0.0, 1.0],
-        [0.0, 0.0, 1.0, 1.0],
+        [1.0, 0.0, 0.0, 0.0],
+        [0.0, 1.0, 0.0, 0.0],
+        [0.0, 0.0, 1.0, 0.0],
     ]
 )
-rays = sample_rays_for_image_render(camera_origin, camera_params, 10, 10)
-positions_along_rays = sample_positions_along_rays(rays, 0.0, 1.0, 10)
+rays = sample_rays_for_image_render(camera_origin, camera_params, 1, 1)
+positions_along_rays = sample_positions_along_rays(rays, 0.0, 1.0, 5)
 print(positions_along_rays)
+
+# fig = plt.figure()
+# ax = fig.add_subplot(projection="3d")
+# flat_positions = positions_along_rays.reshape(-1, 3)
+# ax.scatter(
+#     flat_positions[:, 0],
+#     flat_positions[:, 1],
+#     flat_positions[:, 2],
+# )
+fig = plt.figure()
+ax = fig.add_subplot(projection="3d")
+flat_positions = rays.reshape(-1, 6)[:, :3]
+ax.scatter(
+    flat_positions[:, 0],
+    flat_positions[:, 1],
+    flat_positions[:, 2],
+)
+plt.show()
