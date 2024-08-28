de95ee84b2d6b5d27c345e24733e211deb4ad45f
Arno Gobbin <32413451+a-gn@users.noreply.github.com>
2024-08-28 23:07:21 +0200

    more matrixing

--- reimpl_a_gn/voxels/render.py
+++ reimpl_a_gn/voxels/render.py
@@ -4,53 +4,83 @@ import jax.typing as jt
 import jax.lax as lax
 
 
-def get_ray_to_image_coordinates(
-    camera_params: jax.Array, pixel_row: float, pixel_col: float, pixel_size_x: float, pixel_size_y: float
-) -> jax.Array:
+class CameraParams:
+    """Parameters of a pinhole camera."""
+
+    def __init__(self, camera_matrix: jt.ArrayLike, focal_length: float):
+        """Initialize camera parameters.
+
+        @param camera_matrix Camera matrix. Shape: (3, 4).
+        @param focal_length Focal length of the camera.
+        """
+        self.camera_matrix = jnp.array(camera_matrix)
+        if self.camera_matrix.shape != (3, 4):
+            raise ValueError(f"Expected camera matrix to have shape (3, 4), got {self.camera_matrix.shape}")
+        self.focal_length = focal_length
+        self.pixel_size_x = (1 / self.camera_matrix[0, 0]) * focal_length
+        self.pixel_size_y = (1 / self.camera_matrix[1, 1]) * focal_length
+        self._inverse_camera_matrix = None
+
+    def world_points_to_image(self, world_points: jt.ArrayLike) -> jax.Array:
+        return self.camera_matrix @ world_points
+
+    def image_points_to_world(self, image_points: jt.ArrayLike) -> jax.Array:
+        """Compute the direction of a ray from the camera origin to a point in the image.
+
+        @param image_points Image points, pixel coordinates. Shape: (point_count, 2). Last axis: x, y.
+        @return Point coordinates in the world coordinate system. Shape: (point_count, 3). Last axis: x, y, z.
+        """
+        image_points = jnp.array(image_points)
+        image_points_homogeneous = jnp.concatenate([image_points, jnp.ones((image_points.shape[0], 1))], axis=1)
+        inverse_camera_matrix = self.compute_inverse_camera_matrix()
+        return (inverse_camera_matrix @ image_points_homogeneous.transpose()).transpose()
+
+    def compute_inverse_camera_matrix(self):
+        if self._inverse_camera_matrix is None:
+            self._inverse_camera_matrix = jnp.linalg.pinv(self.camera_matrix)
+        return self._inverse_camera_matrix
+
+
+def get_ray_to_image_coordinates(camera_params: CameraParams, pixel_row: float, pixel_col: float) -> jax.Array:
     """Compute the direction of a ray from the camera origin to a point in the image.
 
     Assumes that the camera is at the origin and looks along the z-axis.
 
-    @param camera_params Camera matrix. Shape: (3, 4).
+    @param camera_params Camera parameters.
     @param pixel_row Row index of the pixel. Can be a float to represent subpixel positions.
     @param pixel_col Column index of the pixel. Can be a float to represent subpixel positions.
-    @param pixel_size_x Width of a pixel in the image plane.
-    @param pixel_size_y Height of a pixel in the image plane.
     @return Unit vector in the direction of the ray from camera origin to a point in the image plane.
     Shape: (image_height, image_width, 3). Last axis: x, y, z.
     """
-    camera_params = jnp.array(camera_params)
-    focal_length = camera_params[0, 0] * pixel_size_x
-    principal_point = jnp.array([0, 0, focal_length])
+    principal_point_pixels = jnp.array([])
+    principal_point_world = jnp.array([0, 0, camera_params.focal_length])
     pixel_center = jnp.array([
-        principal_point[0] + pixel_size_x * pixel_col,
-        principal_point[1] - pixel_size_y * pixel_row,
-        principal_point[2],
+        principal_point_world[0] + camera_params.pixel_size_x * pixel_col,
+        principal_point_world[1] + camera_params.pixel_size_y * pixel_row,
+        principal_point_world[2],
     ])
     origin_to_pixel = pixel_center
+    assert jnp.linalg.norm(origin_to_pixel, ord=2) > 0.0
     origin_to_pixel = origin_to_pixel / jnp.linalg.norm(origin_to_pixel, ord=2)
     return origin_to_pixel
 
 
-def sample_rays_for_image_render(
-    camera_origin: jax.Array,
-    camera_params: jax.Array,
+def sample_rays_towards_all_pixels(
+    camera_params: CameraParams,
     image_height: int,
     image_width: int,
 ) -> jax.Array:
     """Sample parameters of rays through a pinhole camera, which can be used to render an image.
 
-    @param camera_origin Position of the camera. Shape: (4,).
     @param camera_params Camera matrix. Shape: (3, 4).
     @param image_height Pixel row count of the image we want to render.
     @param image_width Pixel column count of the image we want to render.
     @return Ray parameters. Shape: (image_height, image_width, 6). Second axis: x, y, z, dx, dy, dz.
     """
-    camera_origin = camera_origin[:3] / camera_origin[3]
     ray_coords = jnp.zeros((image_height, image_width, 6), dtype=jnp.float32)
     for row_i in range(image_height):
         for col_i in range(image_width):
-            ray_direction = get_ray_to_image_coordinates(camera_params, row_i, col_i, 1.0, 1.0)
+            ray_direction = get_ray_to_image_coordinates(camera_params, row_i, col_i)
             ray_coords = ray_coords.at[row_i, col_i, 3:6].set(ray_direction)
     return ray_coords
 
@@ -74,7 +104,7 @@ def sample_positions_along_rays(
         (jnp.linalg.norm(ray_directions, axis=-1, ord=2) ** 1 / 2), 2
     )
     distance_interval = (far_distance - near_distance) / pos_per_ray
-    for pos_i in range(pos_per_ray + 1):
+    for pos_i in range(1, pos_per_ray + 1):
         result = result.at[..., pos_i, :].set(
             ray_origins
             + norm_ray_directions * (near_distance + pos_i * distance_interval)
--- test_renderer.py
+++ test_renderer.py
@@ -2,38 +2,75 @@ import matplotlib.pyplot as plt
 import jax.numpy as jnp
 
 from reimpl_a_gn.voxels.render import (
-    sample_rays_for_image_render,
+    sample_rays_towards_all_pixels,
     sample_positions_along_rays,
     blend_ray_features,
+    CameraParams,
 )
 
 
-camera_origin = jnp.array([0.0, 0.0, 0.0, 1.0])
-camera_params = jnp.array(
-    [
-        [1.0, 0.0, 0.0, 0.0],
-        [0.0, 1.0, 0.0, 0.0],
+# camera_params = CameraParams(
+#     jnp.array([
+#         [2.0, 0.0, 0.0, -2.0],
+#         [0.0, 1.0, 0.0, 1.0],
+#         [0.0, 0.0, 1.0, 3.0],
+#     ]),
+#     4.0,
+#     5.0,
+#     5.0,
+# )
+camera_params = CameraParams(
+    jnp.array([
+        [4.0, 0.0, 0.0, 0.0],
+        [0.0, 4.0, 0.0, 0.0],
         [0.0, 0.0, 1.0, 0.0],
-    ]
+    ]),
+    2.0,
 )
-rays = sample_rays_for_image_render(camera_origin, camera_params, 10, 10)
-positions_along_rays = sample_positions_along_rays(rays, 0.0, 1.0, 5)
-print(positions_along_rays)
 
-fig = plt.figure()
-ax = fig.add_subplot(projection="3d")
-flat_positions = positions_along_rays.reshape(-1, 3)
-ax.scatter(
-    flat_positions[:, 0],
-    flat_positions[:, 1],
-    flat_positions[:, 2],
+print(camera_params.image_points_to_world(jnp.array([[0.0, 0.0]])))
+
+all_x, all_y = pixel_grid = jnp.meshgrid(jnp.arange(1, 5, 0.5), jnp.arange(1, 5, 0.5))
+all_grid_points = jnp.stack([all_x, all_y], axis=-1).reshape(-1, 2)
+fig_2d = plt.figure()
+plt.scatter(all_grid_points[:, 0], all_grid_points[:, 1])
+
+all_grid_points_world = camera_params.image_points_to_world(all_grid_points)
+all_grid_points_world_non_hom = jnp.stack(
+    [
+        all_grid_points_world[:, 0] / all_grid_points_world[:, 3],
+        all_grid_points_world[:, 1] / all_grid_points_world[:, 3],
+        all_grid_points_world[:, 2] / all_grid_points_world[:, 3],
+    ],
+    axis=-1,
 )
+fig_3d = plt.figure()
+ax = fig_3d.add_subplot(projection="3d")
+ax.scatter(
+    all_grid_points_world_non_hom[:, 0],
+    all_grid_points_world_non_hom[:, 1],
+    all_grid_points_world_non_hom[:, 2],
+)
+plt.show()
+
+# rays = sample_rays_towards_all_pixels(camera_params, 10, 10)
+# positions_along_rays = sample_positions_along_rays(rays, 0.0, 1.0, 5)
+# print(positions_along_rays)
+
 # fig = plt.figure()
 # ax = fig.add_subplot(projection="3d")
-# flat_positions = rays.reshape(-1, 6)[:, :3]
+# flat_positions = positions_along_rays.reshape(-1, 3)
 # ax.scatter(
 #     flat_positions[:, 0],
 #     flat_positions[:, 1],
 #     flat_positions[:, 2],
 # )
-plt.show()
+# # fig = plt.figure()
+# # ax = fig.add_subplot(projection="3d")
+# # flat_positions = rays.reshape(-1, 6)[:, :3]
+# # ax.scatter(
+# #     flat_positions[:, 0],
+# #     flat_positions[:, 1],
+# #     flat_positions[:, 2],
+# # )
+# plt.show()
