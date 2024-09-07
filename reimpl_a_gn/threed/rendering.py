502a0554eec8a5e7bdb026afb96aebf130a9abdf
Arno Gobbin <32413451+a-gn@users.noreply.github.com>
2024-09-07 23:33:20 +0200

    some changes in camera matrices
    will probably rewrite that to handle world/camera transformations

--- reimpl_a_gn/threed/rendering.py
+++ reimpl_a_gn/threed/rendering.py
@@ -8,44 +8,48 @@ import numpy  # some operations aren't supported on jax-metal
 class CameraParams:
     """Parameters of a pinhole camera."""
 
-    def __init__(self, camera_matrix: jt.ArrayLike, focal_length: float):
+    def __init__(
+        self,
+        extrinsic_matrix: jt.ArrayLike,
+        intrinsic_matrix: jt.ArrayLike,
+        focal_length: float,
+    ):
         """Initialize camera parameters.
 
-        @param camera_matrix Camera matrix. Shape: (3, 4).
+        @param intrinsic_matrix Intrinsic parameters, from camera frame to image coordinates. Shape: (3, 4).
+        @param extrinsic_matrix Extrinsic parameters, from world frame to camera frame. Shape: (4, 4).
         @param focal_length Focal length of the camera.
         """
-        self.camera_matrix = jnp.array(camera_matrix)
-        if self.camera_matrix.shape != (3, 4):
-            raise ValueError(f"Expected camera matrix to have shape (3, 4), got {self.camera_matrix.shape}")
-        self.focal_length = focal_length
-        self.pixel_size_x = (1 / self.camera_matrix[0, 0]) * focal_length
-        self.pixel_size_y = (1 / self.camera_matrix[1, 1]) * focal_length
-        self._inverse_camera_matrix = None
+        self.world_to_camera = jnp.array(extrinsic_matrix)
+        if self.world_to_camera.shape != (3, 4):
+            raise ValueError(
+                f"Expected camera matrix to have shape (3, 4), got {self.world_to_camera.shape}"
+            )
+        self.camera_to_world = jnp.array(numpy.linalg.inv(self.world_to_camera))
 
-    def camera_points_to_image(self, camera_points: jt.ArrayLike) -> jax.Array:
-        return self.camera_matrix @ camera_points
+        self.camera_to_image = jnp.array(intrinsic_matrix)
+        if self.camera_to_image.shape != (3, 4):
+            raise ValueError(
+                f"Expected camera matrix to have shape (3, 4), got {self.camera_to_image.shape}"
+            )
+        self.image_to_camera = jnp.array(numpy.linalg.pinv(self.camera_to_image))
+
+        self.focal_length = focal_length
+        self.pixel_size_x = (1 / self.camera_to_image[0, 0]) * focal_length
+        self.pixel_size_y = (1 / self.camera_to_image[1, 1]) * focal_length
 
     def image_points_to_camera(self, image_points: jt.ArrayLike) -> jax.Array:
         """Compute the direction of a ray from the camera origin to a point in the image.
+
+        This is a function because we go through homogeneous coordinates, but the input coordinates are in pixels.
 
         @param image_points Image points, pixel coordinates. Shape: (point_count, 2). Last axis: x, y.
         @return Point coordinates in the camera coordinate system. Shape: (point_count, 3). Last axis: x, y, z.
         """
         image_points = jnp.array(image_points)
         image_points_homogeneous = jnp.concatenate([image_points, jnp.ones((image_points.shape[0], 1))], axis=1)
-        inverse_camera_matrix = self.compute_inverse_camera_matrix()
+        inverse_camera_matrix = self.image_to_camera
         return (inverse_camera_matrix @ image_points_homogeneous.transpose()).transpose()
-
-    def compute_inverse_camera_matrix(self):
-        """Compute the pseudoinverse of the camera matrix, which computes ray direction from image point.
-
-        This goes through numpy on CPU because jax-metal doesn't support the pinv operation as of writing.
-        """
-        if self._inverse_camera_matrix is None:
-            self._inverse_camera_matrix = jnp.array(
-                numpy.linalg.pinv(self.camera_matrix)
-            )  # pinv not supported by jax-metal on 24/09/01
-        return self._inverse_camera_matrix
 
 
 def get_ray_to_image_coordinates(camera_params: CameraParams, pixel_row: float, pixel_col: float) -> jax.Array:
