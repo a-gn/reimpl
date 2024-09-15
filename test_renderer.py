e314a4bb253557fc2bd0cedd9c02287499acd647
Arno Gobbin <32413451+a-gn@users.noreply.github.com>
2024-09-15 14:48:08 +0200

    plot rays towards pixels with one camera

--- test_renderer.py
+++ test_renderer.py
@@ -206,4 +206,69 @@ def test_load_data():
     plot_cameras(data.cameras, data.i_test)
 
 
-test_load_data()
+# test_load_data()
+
+
+def test_sample_rays_from_actual_cameras():
+    data = load_synthetic_nerf_dataset(
+        "/Volumes/ESSB/research/datasets/nerfs/synthetic nerf dataset (original paper)/NeRF_Data/nerf_llff_data/flower"
+    )
+    camera_params = data.cameras[0]
+    rays = sample_rays_towards_all_pixels(camera_params, 10, 10)
+    rays = sample_regular_positions_along_rays(rays, 0, 10, 5)
+
+    all_x, all_y = jnp.meshgrid(jnp.arange(-2, 3, 0.7), jnp.arange(-5, 5, 0.5))
+    all_grid_points = jnp.stack([all_x, all_y], axis=-1).reshape(-1, 2)
+
+    all_grid_points_camera_frame = camera_params.image_points_to_camera(all_grid_points)
+    all_rays_towards_grid_points = jnp.concatenate(
+        [
+            jnp.zeros([all_grid_points_camera_frame.shape[0], 3]),
+            all_grid_points_camera_frame[:, :3],
+        ],
+        1,
+    )
+
+    def plot_pixels(ax: plt.Axes, points: jnp.ndarray):  # type: ignore
+        ax.scatter(points[:, 0], points[:, 1], points[:, 2])
+
+    regularly_sampled_rays = sample_regular_positions_along_rays(
+        all_rays_towards_grid_points, 0.5, 3.0, 3
+    )
+    print(regularly_sampled_rays)
+    fig_rays, axes = plt.subplots(1, 2, subplot_kw={"projection": "3d"})
+    ax_rays_regular, ax_rays_nerf = axes
+    ax_rays_regular.scatter([0], [0], [0], color="red")
+    ax_rays_nerf.scatter([0], [0], [0], color="red")
+
+    ax_rays_regular.set_title("rays towards pixels, regular sampling")
+    ax_rays_regular.text(1, 0, 0, "x-axis", "x")  # type: ignore
+    ax_rays_regular.text(0, 1, 0, "y-axis", "y")  # type: ignore
+    ax_rays_regular.text(0, 0, 1, "z-axis", "z")  # type: ignore
+    flat_positions = regularly_sampled_rays.reshape(-1, 3)
+    ax_rays_regular.scatter(
+        flat_positions[:, 0],
+        flat_positions[:, 1],
+        flat_positions[:, 2],
+    )
+    plot_pixels(ax_rays_regular, all_grid_points_camera_frame)
+
+    nerf_sampled_rays = sample_nerf_rendering_positions_along_rays(
+        all_rays_towards_grid_points, 0.5, 3.0, 3, jax.random.PRNGKey(0)
+    )
+    ax_rays_nerf.set_title("rays towards pixels, NeRF (bins) sampling")
+    ax_rays_nerf.text(1, 0, 0, "x-axis", "x")  # type: ignore
+    ax_rays_nerf.text(0, 1, 0, "y-axis", "y")  # type: ignore
+    ax_rays_nerf.text(0, 0, 1, "z-axis", "z")  # type: ignore
+    flat_positions = nerf_sampled_rays.reshape(-1, 3)
+    ax_rays_nerf.scatter(
+        flat_positions[:, 0],
+        flat_positions[:, 1],
+        flat_positions[:, 2],
+    )
+    plot_pixels(ax_rays_nerf, all_grid_points_camera_frame)
+
+    plt.show()
+
+
+test_sample_rays_from_actual_cameras()
