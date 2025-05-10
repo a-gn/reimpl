7098fbdb51078a97a154cec8afd44084ceed7030
Arno Gobbin <32413451+a-gn@users.noreply.github.com>
2025-05-10 20:58:39 +0200

    set up nbstripout to clear notebook output when committing

--- .devcontainer/post-create-commands.sh
+++ .devcontainer/post-create-commands.sh
@@ -1,6 +1,11 @@
-#!/usr/bin/bash
+#!/bin/bash
 
+echo "Configuring git user identity..."
 git config --global user.name "Arno Gobbin"
 git config --global user.email "32413451+a-gn@users.noreply.github.com"
 
+echo "Installing project with uv..."
 uv sync --dev
+
+echo "Setting up nbstripout to clear the output of notebook cells before committing..."
+nbstripout --install
