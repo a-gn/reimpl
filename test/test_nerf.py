fixes in fine sampling distribution computation, test not yet correct

test/test_nerf.py
@@ -21,11 +21,33 @@ def test_pass_data_through_fine_mlp():
 
 
 def test_compute_fine_sampling_distribution():
-    densities = jnp.array([[0.5, 0.7, 4.5, 0.0, 3.0]])
+    densities = jnp.array([[0.5, 0.7, 4.5, 0.0, 162.8]])
     sampling_positions = jnp.array([[0.0, 0.3, 1.5, 100.3, 102.0]])
-    expected_distribution = jnp.zeros(len(densities) - 1, dtype=float)  # TODO
+    expected_distribution = jnp.zeros(
+        (sampling_positions.shape[0], sampling_positions.shape[1] - 1), dtype=float
+    )
+    cumulative_transmittance = jnp.cumulative_sum(
+        -densities[:, :-1] * (sampling_positions[:, 1:] - sampling_positions[:, :-1]),
+        axis=1,
+        include_initial=True,
+    )
+    for interval_index in range(0, sampling_positions.shape[1] - 1):
+        expected_distribution = expected_distribution.at[:, interval_index].set(
+            cumulative_transmittance[:, interval_index]
+            * (
+                1
+                - jnp.exp(
+                    -densities[:, interval_index]
+                    * (
+                        sampling_positions[:, interval_index + 1]
+                        - sampling_positions[:, interval_index]
+                    )
+                )
+            )
+        )
 
     computed_distribution = nerf.compute_fine_sampling_distribution(
         densities, sampling_positions
     )
+    print(f"computed:\n{computed_distribution}\nexpected:\n{expected_distribution}")
     assert jnp.allclose(computed_distribution, expected_distribution)