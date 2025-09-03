99769cd91ef6f93478960b8866d22821f853f436
Arno Gobbin <32413451+a-gn@users.noreply.github.com>
2025-09-03 12:18:42 +0200

    coarse prediction test script

--- scripts/sampling.py
+++ scripts/sampling.py
@@ -4,10 +4,22 @@ import jax.numpy as jnp
 import jax.random as rand
 import kagglehub
 import matplotlib.pyplot as plt
+from flax import nnx
 
 from reimpl_a_gn.dataset.synthetic_nerf_dataset import load_synthetic_nerf_dataset
-from reimpl_a_gn.threed.nerf import sample_coarse_mlp_inputs
+from reimpl_a_gn.threed.nerf import (
+    CoarseMLP,
+    FineMLP,
+    compute_fine_sampling_distribution,
+    compute_nerf_positional_encoding,
+    sample_coarse_mlp_inputs,
+)
 from reimpl_a_gn.threed.rendering import sample_rays_towards_pixels
+
+POSITIONAL_ENCODING_COMPONENTS_COARSE = 5
+POSITIONAL_ENCODING_COMPONENTS_FINE = 6
+
+RNGS = nnx.Rngs(7)
 
 
 def get_flower_dataset():
@@ -38,12 +50,22 @@ prng_key = rand.key(7)
 
 prng_key, coarse_sampling_key = rand.split(prng_key)
 coarse_points = sample_coarse_mlp_inputs(rays, 0.01, 0.1, 5, coarse_sampling_key)
-print(coarse_points)
-ax = plt.figure().add_subplot(projection="3d")
-ax.scatter(
-    coarse_points.reshape(-1, 4)[:, 0] / coarse_points.reshape(-1, 4)[:, 3:4],
-    coarse_points.reshape(-1, 4)[:, 1] / coarse_points.reshape(-1, 4)[:, 3:4],
-    coarse_points.reshape(-1, 4)[:, 2] / coarse_points.reshape(-1, 4)[:, 3:4],
+# print(coarse_points)
+# ax = plt.figure().add_subplot(projection="3d")
+# ax.scatter(
+#     coarse_points.reshape(-1, 4)[:, 0] / coarse_points.reshape(-1, 4)[:, 3:4],
+#     coarse_points.reshape(-1, 4)[:, 1] / coarse_points.reshape(-1, 4)[:, 3:4],
+#     coarse_points.reshape(-1, 4)[:, 2] / coarse_points.reshape(-1, 4)[:, 3:4],
+# )
+# plt.show()
+# del coarse_sampling_key
+
+
+encoded_coarse_points = compute_nerf_positional_encoding(
+    coarse_points, POSITIONAL_ENCODING_COMPONENTS_COARSE
 )
-plt.show()
-del coarse_sampling_key
+coarse_mlp = CoarseMLP(
+    POSITIONAL_ENCODING_COMPONENTS_COARSE * 2, (32, 32), 4, rngs=RNGS
+)
+coarse_predictions = coarse_mlp(encoded_coarse_points)
+print(coarse_predictions)
